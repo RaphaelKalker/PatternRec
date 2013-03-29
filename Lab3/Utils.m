bafa6ab9be29d9c70bf62dbf49eba4e45ec2680f
Matthew Chong <matt.chong45@gmail.com>
2013-03-29 19:10:08 -0400

    added part 3. needs some work switching between features

--- Lab3/Utils.m
+++ Lab3/Utils.m
@@ -1,26 +1,30 @@
 classdef Utils
     methods (Static)
 
-         function grid = CreateGrid(means, datapoints, variances) 
-                dx = 5; % step size
+         function [grid, xVals, yVals] = CreateGrid(datapoints) 
+                dx = 0.01; % step size
                 maxY = max(datapoints(:, 2));
                 maxX = max(datapoints(:, 1));
                 minX = min(datapoints(:, 1));
                 minY = min(datapoints(:, 2));
                 xVals = [minX:dx:maxX];
                 yVals = [minY:dx:maxY];
-                
+
                 grid = zeros(length(xVals),length(yVals));
-                
-                for k=1:length(xVals)
-                    for j=1:length(yVals)
-                            grid(k,j) = Utils.GED_Classifier(means, [xVals(k), yVals(j)], variances); 
-                    end
+        end
+
+        function cont = GED_Contour(grid, xVals, yVals, means, datapoints, variances)
+            for k=1:length(xVals)
+                for j=1:length(yVals)
+                    grid(k,j) = Utils.GED_Classifier(means, [xVals(k), yVals(j)], variances); 
                 end
-                contour(xVals, yVals, grid, 3, 'k');
+            end
+            cont = grid;
+            contour(xVals, yVals, grid, 10);
         end
 
         function class = GED_Classifier(means, point, variances)
+
             mean = [];
             dist = [];
             variance = [];
@@ -29,14 +33,19 @@ classdef Utils
             for k=1:numOfMeans
                 mean{k} = [means(k, 1), means(k, 2)];
                 variance{k} = [variances(k+count,1) variances(k+count+1,2); variances(k+count+1, 1) variances(k+count+1, 2)];
-                dist{k} = sqrt((point - cell2mat(mean(k)))*inv(variances(k))*(point - cell2mat(mean(k)))');
+                dist = sqrt((point - cell2mat(mean(k)))*inv(variances(k))*(point - cell2mat(mean(k)))');
                 count = count +1;
+                if (k ==1)
+                    distances = [dist];
+                else
+                    distances = [distances dist];
+                end
             end
             
-            [~,class] = min([dist(1) dist(2) dist(3)]);
+            [~,class] = min(distances);
                 
         end
-
+        
         function [mu, sig] = learnParams(data, N)
             mu = (1/N)*sum(data);
             temp = [0 0; 0 0];
@@ -44,6 +53,19 @@ classdef Utils
                 temp = temp + (data(k,:) - mu)'*(data(k,:) - mu);
             end
             sig = (1/N).*temp;
+        end
+
+        function confPoints = MICD_Check(means, testPts, variances)
+
+            confPoints = zeros(10,10); 
+
+            dists = [];
+
+            for k=1:length(testPts(:,1))
+                index = testPts(k, 3);
+                class = Utils.GED_Classifier(means, [testPts(k,1) testPts(k,2)], variances );
+                confPoints(index, class) = confPoints(index, class) + 1;
+            end
         end
 
     end
--- Lab3/lab3.m
+++ Lab3/lab3.m
@@ -4,31 +4,54 @@ close all
 
 load feat.mat
 
-feature = f2
-temp = 0;
+feature = f32;
+featureTest = f32t;
+%-------------------------------------------------------------
+% Part 3 - Labelled Classification
+%-------------------------------------------------------------
 
+% extract datapoints and learn class means and variances
 for k=1:10
 	count = 0;
 	for j=(16*k-15):16*k
 		if (feature(3,j) == k)
 			if (count == 0)
-				data = [feature(1, j) feature(2, j)]
+				data = [feature(1, j) feature(2, j)];
+				testData = [featureTest(1, j) featureTest(2, j), featureTest(3,j)];
 			else
-				data = [data; feature(1, j) feature(2, j)]
+				data = [data; feature(1, j) feature(2, j)];
+				testData = [testData; featureTest(1, j) featureTest(2, j), featureTest(3,j)];
 			end
 			count = 1;
 		end
 	end
-	datapoints{k} = data;
-end
+	[mu, sig] = Utils.learnParams(data, 16);
+	[testMu, testSig] = Utils.learnParams([testData(:, 1) testData(:,2)], 16);
 
-for k=1:10
-	[mu, sig] = Utils.learnParams(cell2mat(datapoints(k)), 16)
 	if (k == 1)
 		means = mu;
+		testMeans = testMu;
 		variances = sig;
+		testVariances = testSig;
+		datapoints = [data];
+		testDatapoints = [testData];
 	else
 		means = [means; mu];
+		testMeans = [testMeans; testMu];
 		variances = [variances; sig];
+		testVariances = [testVariances; testSig];
+		datapoints = [datapoints; data];
+		testDatapoints = [testDatapoints; testData];
 	end
+	
 end
+
+% MICD Classifier
+[grid, xVals, yVals] = Utils.CreateGrid(datapoints);
+cont = Utils.GED_Contour(grid, xVals, yVals, means, datapoints, variances);
+confPoints = Utils.MICD_Check(testMeans, testDatapoints, testVariances)
+
+
+
+
+
